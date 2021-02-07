d196fafde243c3d9dbd3792ce9a157fdd9850b32
luk-str <lstrociak@gmail.com>
2021-02-07 14:28:06 +0100

    Add styling to intro page

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,11 +1,30 @@
 import { ReactElement } from "react";
+import styled from "styled-components";
+
+const IntroWrapper = styled.section`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const IntroHeader = styled.h1`
+  font-size: 3.815rem;
+  margin-bottom: 1rem;
+`;
+
+const IntroText = styled.p`
+  font-size: 1.25rem;
+`;
 
 const Intro = (): ReactElement => {
   return (
-    <>
-      <h1>Sandwich Cemetery</h1>
-      <p>Coming soon...</p>
-    </>
+    <IntroWrapper>
+      <IntroHeader>Sandwich Cemetery</IntroHeader>
+      <IntroText>Coming soon</IntroText>
+    </IntroWrapper>
   );
 };
 
