fb0aab01d48723e40324956ecebdda3b53212c86
luk-str <lstrociak@gmail.com>
2021-02-07 11:59:06 +0100

    Reset project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sandwich Cemetery</title>
+      </Head>
+      <h1>Sandwich Cemetery</h1>
+    </>
+  );
+}
